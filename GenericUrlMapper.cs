5e30524c99f16a62343de0c004735c958548352b
Rodrick Chapman <noblethrasher@gmail.com>
2011-05-15 20:09:23 -0500

    first post!

---
+++ GenericUrlMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Harmony
+{
+    abstract class UrlMappingModule<TRoot> : IHttpModule where TRoot : Controller, new()
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += delegate
+            {
+                Controller controller = new TRoot();
+                var path = context.Context.Request.Path.Split('/');
+
+                foreach (var segment in path)
+                    controller = controller.HandleMessage(segment);
+
+                context.Context.RemapHandler(controller);
+            };
+        }
+
+        public void Dispose() { }
+    }
+
+    
+}
