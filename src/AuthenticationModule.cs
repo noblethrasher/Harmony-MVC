48675f5a9f164ec734119b67e5f0245321138683
Rodrick Chapman <noblethrasher@gmail.com>
2011-05-15 23:05:28 -0500

    added auth module

---
+++ src/AuthenticationModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Harmony
+{
+    public class AuthenticationModule : IHttpModule
+    {
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.AuthenticateRequest += delegate
+            {
+                if (DateTime.Now == DateTime.Now)
+                {
+
+                }
+                else
+                {
+                    var username = context.Request.Form["username"];
+                    var password = context.Request.Form["password"];
+
+
+                    //TODO: Add check for secure connection...
+                    if (username != null && password != null)
+                    {
+
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Security.Principal;
+
+namespace Harmony
+{
+    class User : IPrincipal
+    {
+        List<string> Roles { get; private set; }
+
+        public User(string Name, string AuthenticationType, bool IsAuthenticated, IEnumerable<string> Roles)
+        {
+            _id = new _Identity(Name, AuthenticationType, IsAuthenticated);
+            this.Roles = new List<string>(Roles);
+        }
+        
+        class _Identity : IIdentity
+        {
+
+            public _Identity(string Name, string AuthenticationType, bool IsAuthenticated)
+            {
+                this.Name = Name;
+                this.AuthenticationType = AuthenticationType;
+                this.IsAuthenticated = IsAuthenticated;
+            }
+            
+            
+            public string AuthenticationType
+            {
+                get;
+                private set;
+            }
+
+            public bool IsAuthenticated
+            {
+                get;
+                private set;
+            }
+
+            public string Name
+            {
+                get;
+                private set;
+            }
+        }
+
+        _Identity _id;
+        public IIdentity Identity
+        {
+            get { return this._id; }            
+        }
+
+        public bool IsInRole(string role)
+        {
+            return Roles != null && Roles.Contains(role);
+        }
+    }
+}
